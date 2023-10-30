bc958e1f16b434750144106546a546fec003e2ac
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-10-30 20:11:54 +0530

    feat: add user route

---
+++ models/userModel.go
@@ -0,0 +1,15 @@
+package userModel
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Users []User
